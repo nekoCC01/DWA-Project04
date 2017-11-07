a9103c613701d8751a3e6a7845e684c7562c0208
Daniel Kasai <danielkasai2012@gmail.com>
2017-11-07 14:29:45 +0100

    database configuration, tables added

---
+++ database/migrations/2017_11_07_130906_create_arguments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArgumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arguments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->text('assumption');
+            $table->text('conclusion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arguments');
+    }
+}
