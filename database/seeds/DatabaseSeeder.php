5b025e23804a51d8b644b069ff37cad3ed429818
Daniel Kasai <danielkasai2012@gmail.com>
2017-11-14 22:36:19 +0100

    better DatabaseSeeder syntax

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,20 +2,20 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-	    $this->call(QuotesTableSeeder::class);
-	    $this->call(ConceptsTableSeeder::class);
-	    $this->call(DefinitionsTableSeeder::class);
-	    $this->call(ArgumentsTableSeeder::class);
-	    $this->call(PhilosophersTableSeeder::class);
-	    $this->call(WorksTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$this->call( [
+			QuotesTableSeeder::class,
+			ConceptsTableSeeder::class,
+			DefinitionsTableSeeder::class,
+			ArgumentsTableSeeder::class,
+			PhilosophersTableSeeder::class,
+			WorksTableSeeder::class
+		] );
+	}
 }
